40a796dd8490f03bbafda4803e2bc15bb686766c
Daniil Dybka <danil.dybko@gmail.com>
2024-09-30 15:54:09 +0300

    Load data

---
+++ js/add-data.js
@@ -0,0 +1,47 @@
+const wins = [
+    {
+        title: 'Будущее начинается сегодня', desc: 'Конкурс от Московского Авиационного Института.', imageFancybox: 'mai1', images: ['/img/mai/2.jpg']
+    },
+    {
+        title: 'НТИ Олимпиада', desc: 'Награждение финалистов в офисе 1C.', imageFancybox: '1c', images: ['/img/1c/1.jpg']
+    },
+    {
+        title: 'Startup House Universities', desc: 'Конкурс от института «Иннополис», г.Казань.', imageFancybox: 'innopolis', images: ['/img/innopolis/1.jpg', '/img/innopolis/2.jpg', '/img/innopolis/3.jpg']
+    },
+    {
+        title: 'Разведка боем', desc: 'Конкурс от института «Иннополис», г.Казань.', imageFancybox: 'innopolis', images: ['/img/innopolis/1.jpg', '/img/innopolis/2.jpg', '/img/innopolis/3.jpg']
+    },
+    {
+        title: 'Startup House Universities', desc: 'Конкурс от института «Иннополис», г.Казань.', imageFancybox: 'innopolis', images: ['/img/innopolis/1.jpg', '/img/innopolis/2.jpg', '/img/innopolis/3.jpg']
+    },
+    {
+        title: 'Startup House Universities', desc: 'Конкурс от института «Иннополис», г.Казань.', imageFancybox: 'innopolis', images: ['/img/innopolis/1.jpg', '/img/innopolis/2.jpg', '/img/innopolis/3.jpg']
+    },
+    {
+        title: 'Startup House Universities', desc: 'Конкурс от института «Иннополис», г.Казань.', imageFancybox: 'innopolis', images: ['/img/innopolis/1.jpg', '/img/innopolis/2.jpg', '/img/innopolis/3.jpg']
+    },
+    {
+        title: 'Startup House Universities', desc: 'Конкурс от института «Иннополис», г.Казань.', imageFancybox: 'innopolis', images: ['/img/innopolis/1.jpg', '/img/innopolis/2.jpg', '/img/innopolis/3.jpg']
+    },
+    {
+        title: 'Startup House Universities', desc: 'Конкурс от института «Иннополис», г.Казань.', imageFancybox: 'innopolis', images: ['/img/innopolis/1.jpg', '/img/innopolis/2.jpg', '/img/innopolis/3.jpg']
+    },
+    {
+        title: 'Студенческая весна', desc: 'Конференция в МГТУ <nobr>им. Н.Э. Баумана</nobr>, кафедры ИУ-9.', imageFancybox: 'baumanka3', images: ['/img/baumanka3/img2.jpg']
+    },
+]
+
+const smi = [
+    {
+        title: 'Название',
+        link: '123'
+    },
+    {
+        title: 'Название',
+        link: '123'
+    },
+    {
+        title: 'Название',
+        link: '123'
+    },
+]
--- js/add.js
+++ js/add.js
@@ -1,34 +1,4 @@
-const wins = [
-    {
-        title: 'Студенческая весна 1', desc: 'Конференция в МГТУ <nobr>им. Н.Э. Баумана</nobr>, кафедры ИУ-9.', imageFancybox: '123', images: ['/img/baumanka3/img2.jpg']
-    },
-    {
-        title: 'Студенческая весна 2', desc: 'Конференция в МГТУ <nobr>им. Н.Э. Баумана</nobr>, кафедры ИУ-9.', imageFancybox: '123', images: ['/img/baumanka3/img2.jpg']
-    },
-    {
-        title: 'Студенческая весна 3', desc: 'Конференция в МГТУ <nobr>им. Н.Э. Баумана</nobr>, кафедры ИУ-9.', imageFancybox: '123', images: ['/img/baumanka3/img2.jpg']
-    },
-    {
-        title: 'Студенческая весна', desc: 'Конференция в МГТУ <nobr>им. Н.Э. Баумана</nobr>, кафедры ИУ-9.', imageFancybox: 'baumanka3', images: ['/img/baumanka3/img2.jpg']
-    },
-]
-
-const smi = [
-    {
-        title: 'Название',
-        link: '123'
-    },
-    {
-        title: 'Название',
-        link: '123'
-    },
-    {
-        title: 'Название',
-        link: '123'
-    },
-]
-
-function AddWins() {
+function AddWins(lang) {
     function winHtml({ id, title, desc, imageFancybox, images }) {
         return `<div class="item--block" id="k${id}">
                 <div class="container px-0">
@@ -39,9 +9,12 @@ function AddWins() {
                 </div>
                 <div class="row g-0">
                     <div class="col-md-12 col-sm-12">
-                        ${images.map(image => `<a data-fancybox="${imageFancybox}" data-src="${image}">
-                            <img src="${image}" class="item--img-top pointer img-fluid w-100 m-0 p-0" alt="${title}">
-                        </a>`).join('\n') }
+                        ${images.map((image, index) => {
+                            let classImg = 'once'
+                            return `<a data-fancybox="${imageFancybox}" data-src="${image}">
+                            <img src="${image}" class="item--img-${classImg} pointer img-fluid w-100 m-0 p-0" alt="${title}">
+                        </a>`
+                        }).join('\n')}
                     </div>
                 </div>
             </div>`
@@ -55,12 +28,17 @@ function AddWins() {
 
     for (let i = wins.length - 2; i >= 0; i--) {
         let win = winHtml(wins[i])
-        elementsRoot.insertAdjacentHTML('afterbegin', win)
+        elementsRoot.insertAdjacentHTML('beforeend', win)
     }
 }
 
-function AddSMI() {
+function AddSMI(lang) {
     const root = document.getElementById('')
 }
 
-AddWins()
+function Add(lang) {
+    lang = lang == 'ru' || lang == 'en' ? lang : 'en'
+    console.log('debug: ' + lang)
+    AddSMI(lang)
+    AddWins(lang)
+}
